d94df0565838691abcafd5fd1a4639aefaaab86a
Sahil Shadwal <sahilshadwal@gmail.com>
2024-01-26 14:24:04 +0530

    implemented ping

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::net::TcpListener;
-
+use std::{io::Write, net::TcpListener};
 fn main() {
     println!("Logs from your program will appear here!");
 
@@ -8,8 +7,10 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+            
+            _stream.write_all(b"+PONG\r\n").expect("Could not respond");
             }
             Err(e) => {
                 println!("error: {}", e);
